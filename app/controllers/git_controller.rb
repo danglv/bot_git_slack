a9fdaa2d16fee59bbb83b7545f53e8926c596967
Đăng LV <danglv@topica.edu.vn>
2017-06-09 16:07:17 +0700

    a

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -1,5 +1,7 @@
 class GitController < ApplicationController
   def event
-    render json: {message: params}
+    push = JSON.parse(request.body.read)
+    puts "I got some JSON: #{push.inspect}"
+    render json: {message: push}
   end
 end
