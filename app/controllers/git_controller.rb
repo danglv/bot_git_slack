2ef498ace59b3ba3887ac92b32ab6166cb099097
Đăng LV <danglv@topica.edu.vn>
2017-06-09 16:56:13 +0700

    fdsfds

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -4,6 +4,13 @@ class GitController < ApplicationController
     puts "I got some JSON: #{push.inspect}"
     sender = push["sender"]
     sender_name = sender["login"]
+
+    web_hook_url ="https://hooks.slack.com/services/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"
+    notifier = Slack::Notifier.new web_hook_url do
+      defaults channel: "#general",
+               username: "notifier"
+    end
+    notifier.ping "# For test: #{sender_name} just comment"
     render json: {message: "#{sender_name} just comment"}
   end
 end
